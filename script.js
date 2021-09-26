51587339a34960b649dafb709fc923551a2df80b
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-09-27 01:30:52 +0200

    some changes

--- script.js
+++ script.js
@@ -1,17 +1,65 @@
 const keysContainer = document.querySelector('.keys-container');
 const questionContainer = document.querySelector('.question');
-
-
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-
-const languagesArr = ['Python', 'Java', 'Ruby', 'JavaScript', 'Perl', 'Swift', 'Delphi', 'SQL', 'PHP'];
+const questionAnswer = document.querySelector('.question__answer');
+const questionBgc = document.querySelector('.question__bgc');
+const mistakes = document.getElementById('mistakes');
+const resetBtn = document.querySelector('.keys__btn');
 
 let mistakeCounter = 0;
 
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+
 alphabet.forEach(el => {
-    keysContainer.insertAdjacentHTML("beforeend", `<span class ='letter'> ${el}</span>`);
+    keysContainer.insertAdjacentHTML("beforeend", `<a href="#" class='letter'>${el}</a>`);
 });
 
-let randomNumber = Math.floor(Math.random() * 9);
+// const languagesArr = ['Python', 'Java', 'Ruby', 'JavaScript', 'Perl', 'Swift', 'Delphi', 'SQL', 'PHP'];
 
-questionContainer.insertAdjacentHTML("beforeend", `<p class="question__answer">${languagesArr[randomNumber]}</p>`)
+const languagesArr = ['PPLLAA', 'KKSSDD'];
+
+const randomPuzzleArr = [...(languagesArr[Math.floor(Math.random() * 2)]).toLowerCase()];
+const randomPuzzleDashedArr = [];
+
+for (let i = 0; i < randomPuzzleArr.length; i++) {
+    randomPuzzleDashedArr.push('_')
+}
+
+const updatePuzzle = function () {
+    const word = randomPuzzleDashedArr.join(' ');
+    questionAnswer.textContent = word;
+}
+
+const updateMistakes = function () {
+    mistakeCounter++;
+    mistakes.textContent = mistakeCounter;
+    questionBgc.style.backgroundImage = `url(img/${mistakeCounter}.jpg)`;
+}
+
+console.log(randomPuzzleArr);
+console.log(randomPuzzleDashedArr);
+
+const letters = document.querySelectorAll('.letter')
+
+const checkLetter = function (e) {
+    e.target.classList.add('disabled');
+    const letter = (e.target).textContent;
+
+    if (randomPuzzleArr.includes(letter)) {
+        const letterIndex = randomPuzzleArr.indexOf(letter);
+        randomPuzzleDashedArr[letterIndex] = letter;
+        updatePuzzle();
+    } else {
+        updateMistakes();
+    }
+}
+
+
+letters.forEach(letter => {
+    letter.addEventListener('click', checkLetter)
+})
+
+updatePuzzle();
+
+// wyświetlić informację jeśli uda się wygrać
+// wyświelić informację w razie przegranej 
+// rozwiązać problem dwóch liter
