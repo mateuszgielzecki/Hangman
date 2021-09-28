60e379727fa8fe60acf452b13377f31158bd6d80
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-09-28 20:33:33 +0200

    some changes

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const questionAnswer = document.querySelector('.question__answer');
 const questionBgc = document.querySelector('.question__bgc');
 const mistakes = document.getElementById('mistakes');
 const resetBtn = document.querySelector('.keys__btn');
+const result = document.querySelector('.result');
+const resultWin = document.querySelector('.result-win');
+const resultLose = document.querySelector('.reult-lost')
 
 let mistakeCounter = 0;
 
@@ -27,39 +30,50 @@ for (let i = 0; i < randomPuzzleArr.length; i++) {
 const updatePuzzle = function () {
     const word = randomPuzzleDashedArr.join(' ');
     questionAnswer.textContent = word;
+
+    if (!randomPuzzleDashedArr.includes('_')) {
+        resultWin.style.visibility = 'visible';
+    }
 }
 
 const updateMistakes = function () {
     mistakeCounter++;
     mistakes.textContent = mistakeCounter;
     questionBgc.style.backgroundImage = `url(img/${mistakeCounter}.jpg)`;
+
+    if (mistakeCounter > 6) {
+
+    }
 }
 
 console.log(randomPuzzleArr);
 console.log(randomPuzzleDashedArr);
 
-const letters = document.querySelectorAll('.letter')
+const letters = document.querySelectorAll('.letter');
 
 const checkLetter = function (e) {
     e.target.classList.add('disabled');
     const letter = (e.target).textContent;
 
-    if (randomPuzzleArr.includes(letter)) {
-        const letterIndex = randomPuzzleArr.indexOf(letter);
-        randomPuzzleDashedArr[letterIndex] = letter;
-        updatePuzzle();
+    let flag = false;
+
+    randomPuzzleArr.forEach((el, index) => {
+        if (el === letter) {
+            flag = true
+            randomPuzzleDashedArr[index] = letter;
+            updatePuzzle();
+        }
+    })
+
+    if (flag === true) {
+        return;
     } else {
         updateMistakes();
     }
 }
-
 
 letters.forEach(letter => {
     letter.addEventListener('click', checkLetter)
 })
 
 updatePuzzle();
-
-// wyświetlić informację jeśli uda się wygrać
-// wyświelić informację w razie przegranej 
-// rozwiązać problem dwóch liter
