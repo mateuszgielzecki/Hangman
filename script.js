2ba7bddf2fdd6e773477ae7728fecf50fb8f61af
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-09-29 21:32:12 +0200

    some changes

--- script.js
+++ script.js
@@ -6,57 +6,66 @@ const mistakes = document.getElementById('mistakes');
 const resetBtn = document.querySelector('.keys__btn');
 const result = document.querySelector('.result');
 const resultWin = document.querySelector('.result-win');
-const resultLose = document.querySelector('.reult-lost')
+const resultLose = document.querySelector('.result-lost')
 
 let mistakeCounter = 0;
-
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-
-alphabet.forEach(el => {
-    keysContainer.insertAdjacentHTML("beforeend", `<a href="#" class='letter'>${el}</a>`);
-});
-
-// const languagesArr = ['Python', 'Java', 'Ruby', 'JavaScript', 'Perl', 'Swift', 'Delphi', 'SQL', 'PHP'];
-
-const languagesArr = ['PPLLAA', 'KKSSDD'];
-
+const languagesArr = ['Python', 'Java', 'Ruby', 'JavaScript', 'Perl', 'Swift', 'Delphi', 'SQL', 'PHP'];
 const randomPuzzleArr = [...(languagesArr[Math.floor(Math.random() * 2)]).toLowerCase()];
 const randomPuzzleDashedArr = [];
 
+// Wstawianie liter alfabetu na stronę
+alphabet.forEach(el => {
+    keysContainer.insertAdjacentHTML("beforeend", `<a href="#" class='letter'>${el}</a>`);
+});
+const letters = document.querySelectorAll('.letter');
+
+// Wypełnianie nowej, pustej tablicy "_" zamiast liter z oryginalnego, wylosowanego słowa
 for (let i = 0; i < randomPuzzleArr.length; i++) {
     randomPuzzleDashedArr.push('_')
 }
 
+// Weryfikacja wygranej lub przegranej
+const checkResult = function () {
+    if (mistakeCounter > 5) {
+        resultLose.style.visibility = 'visible';
+        letters.forEach(el => el.classList.add('disabled'));
+
+        // Wstawianie prawidłowego słowa, jeśli przegrana
+        const word = randomPuzzleArr.join(' ');
+        questionAnswer.textContent = word;
+
+    } else if (!randomPuzzleDashedArr.includes('_')) {
+        resultWin.style.visibility = 'visible';
+        letters.forEach(el => el.classList.add('disabled'))
+    }
+}
+
+// Aktualizacja zgadywanego słowa
 const updatePuzzle = function () {
     const word = randomPuzzleDashedArr.join(' ');
     questionAnswer.textContent = word;
 
-    if (!randomPuzzleDashedArr.includes('_')) {
-        resultWin.style.visibility = 'visible';
-    }
+    checkResult()
 }
 
+// Aktualizacja licznika błędów
 const updateMistakes = function () {
     mistakeCounter++;
     mistakes.textContent = mistakeCounter;
     questionBgc.style.backgroundImage = `url(img/${mistakeCounter}.jpg)`;
 
-    if (mistakeCounter > 6) {
-
-    }
+    checkResult()
 }
 
-console.log(randomPuzzleArr);
-console.log(randomPuzzleDashedArr);
-
-const letters = document.querySelectorAll('.letter');
-
+// Weryfikacja czy wybrano prawidłową literę i czy znajduje się w wylosowanym słowie
 const checkLetter = function (e) {
     e.target.classList.add('disabled');
     const letter = (e.target).textContent;
 
     let flag = false;
 
+    // Weryfikacja czy wybrana litera znajduje się w tablicy z wylosowanym słowem
     randomPuzzleArr.forEach((el, index) => {
         if (el === letter) {
             flag = true
@@ -65,15 +74,15 @@ const checkLetter = function (e) {
         }
     })
 
-    if (flag === true) {
-        return;
-    } else {
-        updateMistakes();
-    }
+    //Weryfikacja czy litera była poprawna jeśli tak zakończ działanie, jeśli nie nalicz błąd 
+    if (flag === true) return;
+    else updateMistakes();
+
 }
+
+updatePuzzle();
 
 letters.forEach(letter => {
     letter.addEventListener('click', checkLetter)
 })
 
-updatePuzzle();
